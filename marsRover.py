1ca5f740cd29acf94a859506919dbe22d07974a8
caevh <Harrycave1234@outlook.com>
2020-12-17 11:27:24 +0000

    second commit

--- marsRover.py
+++ marsRover.py
@@ -1,19 +1,65 @@
-class Rover:
+class Thing:
     def __init__(self, x, y, compass):
         self.x = x
         self.y = y
         self.compass = compass
     def move_forward(self):
-        self.x += 1
-    def move_backward(self):
-        self.x -= 1
+        if self.compass == "N":
+            self.x += 1
+        elif self.compass == "S":
+            self.x -= 1
+        elif self.compass == "W":
+            self.y -= 1
+        elif self.compass == "E":
+            self.y += 1
 
+    def move_backward(self):
+        if self.compass == "N":
+            self.x -= 1
+        elif self.compass == "S":
+            self.x += 1
+        elif self.compass == "W":
+            self.y += 1
+        elif self.compass == "E":
+            self.y -= 1
+
+    def turn_left(self):
+        if self.compass == "N":
+            self.compass = "W"
+        elif self.compass == "W":
+            self.compass = "S"
+        elif self.compass == "S":
+            self.compass = "E"
+        elif self.compass == "E":
+            self.compass = "N"
+
+    def turn_right(self):
+        if self.compass == "N":
+            self.compass = "E"
+        elif self.compass == "E":
+            self.compass = "S"
+        elif self.compass == "S":
+            self.compass = "W"
+        elif self.compass == "W":
+            self.compass = "N"
+
+
+
+
+class Martian(Thing):
+    
+    def destroy(self):
+        print("Destroy")
+
+class Rover(Thing):
         
+    def scan(self):
+        print("Scanning")
+
+
 rover1 = Rover(0, 0, "N")
-print(rover1.x)
-rover2 = Rover(1, 1, "N")
-print(rover2.x)
-rover1.move_forward()
-print(rover1.x)
-rover1.move_backward()
-print(rover1.x)
+m = Martian(1, 2, "S")
+players = [rover1, m]
+
+for player in players:
+    print(player.x)
