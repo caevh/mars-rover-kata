00ddda53a81ca42fc8559d838b04c6de51ace63a
caevh <Harrycave1234@outlook.com>
2020-12-17 16:32:49 +0000

    Comments added

--- MarsRoverClass.py
+++ MarsRoverClass.py
@@ -6,6 +6,7 @@ class World:
         self.compass = compass
 
     def x_axis_wrapper(self):
+        # Used to wrap when rover reaches the end of the world on the y axis
         if self.x == 11 and self.compass == "N":
             self.x = -10
         if self.x == 11 and self.compass == "S":
@@ -16,6 +17,7 @@ class World:
             self.x = 10
     
     def y_axis_wrapper(self):
+        # Used to wrap when rover reaches the end of the globe on the y axis
         if self.y == 11 and self.compass == "E":
             self.y = -10
         elif self.y == 11 and self.compass == "W":
@@ -32,6 +34,7 @@ class Rover(World):
 
 
     def move_forward(self):
+        # Moves the rover forward and depending on which way its facing will +,- from x or y axis
         if self.compass == "N":
             self.x += 1
             self.x_axis_wrapper()
@@ -46,6 +49,7 @@ class Rover(World):
             self.y_axis_wrapper()
 
     def move_backward(self):
+        # Moves the rover backwards and depending on which way its facing will +, - from x or y axis
         if self.compass == "N":
             self.x -= 1
             self.x_axis_wrapper()
@@ -60,14 +64,18 @@ class Rover(World):
             self.y_axis_wrapper()
 
     def turn_left(self):
+        # Used to turn the rover left
         directions = {"N": "W", "W": "S", "S": "E", "E": "N"}
         self.compass = directions[self.compass]
 
     def turn_right(self):
+        # Used to turn the rover right
         directions = {"N": "E", "E": "S", "S": "W", "W": "N"}
         self.compass = directions[self.compass]
     
     def commands(self, instructions):
+        # Used to loop through instructions and depending on the command will call the correct method to move or turn the rover
+        # Also has exception incase a wrong str is used as input
         for instruction in instructions:
             if instruction == "f":
                 self.move_forward()
@@ -85,6 +93,6 @@ class Rover(World):
         self.location()
     
     def location(self):
+        # Used to print where the Rover is at the end of the sequence
         print(f"Current coordinates: ({self.x}, {self.y})")
         print(f"direction: {self.compass}")
-
